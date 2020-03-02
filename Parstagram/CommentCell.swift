b6db6358e190bbc6929148db8bdd098ed69f8f46
nguiyim <jnguiyim17@students.desu.edu>
2020-03-01 23:43:56 -0500

    4rd  commit

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Jessica Nguiyim on 3/1/20.
+//  Copyright © 2020 Jessica Nguiyim. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
