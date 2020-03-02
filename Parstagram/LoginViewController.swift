fa4b864c77619bb7eb5ac729d634635bf823c37f
nguiyim <jnguiyim17@students.desu.edu>
2020-03-01 21:16:02 -0500

    first commit

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Jessica Nguiyim on 2/23/20.
+//  Copyright © 2020 Jessica Nguiyim. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Jessica Nguiyim on 3/1/20.
+//  Copyright © 2020 Jessica Nguiyim. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
